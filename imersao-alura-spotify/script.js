93deb152573e76995becc93dba4d9f6dd5a64457
CizeLPC <cizelucas@gmail.com>
2024-01-26 12:22:40 -0300

    finished lesson 4  - added JS code for reading JSON from a fake API and updating HTML accordingly

--- imersao-alura-spotify/script.js
+++ imersao-alura-spotify/script.js
@@ -1,1 +1,36 @@
-console.log("JS working!");
+const resultPlaylist = document.getElementById("result-playlists");
+const resultArtists = document.getElementById("result-artist");
+const searchInput = document.getElementById("search-input");
+
+function requestApi(searchTerm) {
+    
+    const url = `http://localhost:3000/artists?name_like=${searchTerm}`
+    
+    fetch(url)
+        .then((response) => response.json())
+        .then((result) => displayResult(result))
+}
+
+function displayResult(result) {
+    console.log(result);
+    resultPlaylist.classList.add("hidden");
+    const artistName = document.getElementById("artist-name");
+    const artistImg = document.getElementById("artist-img");
+
+    result.forEach(element => {
+        artistName.innerText = element.name;
+        artistImg.src = element.urlImg;
+    });
+
+    resultArtists.classList.remove("hidden");
+}
+
+searchInput.oninput = function() {
+    const searchTerm = searchInput.value;
+    if(searchTerm === ''){
+        resultPlaylist.classList.remove("hidden");
+        resultArtists.classList.add("hidden");
+        return;
+    }
+    requestApi(searchTerm);
+}
